358e6041b5747cc4f4bab70a25b68bd4c57fad8f
VapeCat29 <itsadymythirdacc@gmail.com>
2022-07-10 14:46:26 +0800

    fix maps

--- js/script.js
+++ js/script.js
@@ -14,21 +14,3 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
         document.querySelector('.about .video-container .video').src = src;
     }
 })
-
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat:-4.844378343054717, lng:119.40335814076161 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 17,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
