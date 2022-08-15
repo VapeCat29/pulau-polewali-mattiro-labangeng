fcfcaef58ced3f0d1076063cca0fa4ab5a53b81c
VapeCat29 <itsadymythirdacc@gmail.com>
2022-08-15 18:52:03 +0800

    almost finished

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
         document.querySelector('.about .video-container .video').src = src;
     }
 })
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("content-read-more");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "read less"; 
+      moreText.style.display = "inline";
+    }
+  }
